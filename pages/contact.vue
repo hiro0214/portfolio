c4f73ffb82a90b22467c353ada90860df08e3e09
hiro0214 <leonhurt2@gmail.com>
2020-01-17 03:23:26 +0900

    contact-commit

--- pages/contact.vue
+++ pages/contact.vue
@@ -37,14 +37,33 @@
                 ></v-textarea>
                 <v-btn
                   :disabled="!contactFormValidation.valid"
-                  @click="sendMail()"
+                  @click="clickSubmit()"
                   block
-                  :color="this.$vuetify.theme.themes.dark.primary"
+                  color="info"
                   class="contact-submit"
                 >送信
                 </v-btn>
               </v-form>
             </v-card-text>
+
+            <v-snackbar v-model="snackbar.show" :color="snackbar.color" :timeout="5000" class="font-weight-bold">
+              {{ snackbar.message }}
+            </v-snackbar>
+
+            <v-dialog v-model="dialog" max-width="400">
+              <v-card>
+                <v-card-title>この内容でお問い合わせをしてもよろしいですか？</v-card-title>
+                <v-card-actions :class="`d-flex justify-center`">
+                  <v-btn color="error" @click="dialog = false" class="font-weight-bold">
+                    キャンセル
+                  </v-btn>
+                  <v-btn color="success" @click="sendMail()" class="font-weight-bold">
+                    送信する
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
           </v-card>
         </v-flex>
 
@@ -64,16 +83,17 @@
     line-height:150px;
   }
   &-contents {
-    background:#6a7980;
+    background:#577da1;
     width:60%;
     min-height:500px;
     padding:40px;
     margin:40px auto 0;
     p {
+      font-size:20px;
       text-align:center;
       font-weight:600;
       margin-bottom:20px;
-      color:rgb(226, 226, 226);
+      color:#fff;
     }
   }
 
@@ -105,7 +125,7 @@ export default {
       valid: false,
       nameRules: [
         v => !!v || '名前は必須項目です',
-        v => v.length <= 20 || "20文字以内で入力してください"
+        v => (v && v.length <= 20) || "20文字以内で入力してください"
       ],
       emailRules: [
         v => !!v || 'メールアドレスは必須項目です',
@@ -113,26 +133,42 @@ export default {
       ],
       contentsRules: [
         v => !!v || '内容は必須項目です',
-        v => v.length <= 2000 || "2000文字以内で入力してください"
+        v => (v && v.length <= 2000) || "2000文字以内で入力してください"
       ]
-    }
+    },
+    snackbar: {
+      show: false,
+      color: '',
+      message: ''
+    },
+    dialog: false
   }),
   methods: {
-    sendMail: function () {
+    clickSubmit() {
       if (this.$refs.form.validate()) {
-        const mailer = functions.httpsCallable('sendMail')
-        mailer(this.contactForm)
-          .then(() => {
-            this.formReset()
-          })
-          .catch(err => {
-            console.log(err)
-          })
+        this.dialog = true
       }
+    },
+    sendMail: function () {
+      const mailer = functions.httpsCallable('sendMail')
+      mailer(this.contactForm)
+        .then(() => {
+          this.formReset()
+          this.dialog = false
+          this.showSnackbar('success', 'お問い合わせありがとうございます。送信完了しました')
+        })
+        .catch(err => {
+          this.showSnackbar('error', '送信に失敗しました。')
+          console.log(err)
+        })
+    },
+    showSnackbar (color, message) {
+      this.snackbar.show = true
+      this.snackbar.color = color
+      this.snackbar.message = message
     },
     formReset: function () {
       this.$refs.form.reset()
-      alert('送信しました')
     }
   }
 }
